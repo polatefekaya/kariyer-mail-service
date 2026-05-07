34710ce869f3348e29ef91f39f0e9fc95a68872c
polatefekaya <polatsfekaya@gmail.com>
2026-05-07 18:00:07 +0300

    m broker fix

--- src/Kariyer.Mail.Api/Common/Messaging/MessagingExtensions.cs
+++ src/Kariyer.Mail.Api/Common/Messaging/MessagingExtensions.cs
@@ -40,15 +40,7 @@ public static class MessagingExtensions
 
             x.UsingRabbitMq((context, cfg) =>
             {
-                var rabbitUri = new Uri(rabbitConn);
-                var userInfo = rabbitUri.UserInfo.Split(':');
-                string vhost = string.IsNullOrWhiteSpace(rabbitUri.AbsolutePath.TrimStart('/')) ? "/" : rabbitUri.AbsolutePath;
-
-                cfg.Host(rabbitUri.Host, rabbitUri.Port, vhost, h =>
-                {
-                    h.Username(userInfo[0]);
-                    h.Password(userInfo.Length > 1 ? userInfo[1] : "guest");
-                });
+                cfg.Host(new Uri(rabbitConn));
 
                 cfg.Message<StartBulkEmailJobCommand>(m => m.SetEntityName("MailExchange"));
                 cfg.Message<DispatchEmailCommand>(m => m.SetEntityName("MailExchange"));
